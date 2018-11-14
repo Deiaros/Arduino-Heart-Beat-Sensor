b98bd00de59cd0cdcd66e2f4fb5920f96c652b0c
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-14 11:15:14 +0100

    Update cardio.h

--- Arduino/cardio.h
+++ Arduino/cardio.h
@@ -1,1 +1,18 @@
+/*permet inclure la bibliothèque qu'une seule fois*/
+#ifndef Cardio_h
+#define Cardio_h
+#include "Arduino.h"
 
+class Cardio
+{
+  public : /*La partie "public" est utilisable dans tous les programmes qui utilisent la bibliothèque*/
+  Cardio ();
+  void CardioDetection();
+  void CalculPouls();
+  int valeurPrecedente;
+  long tempsPrecedent;
+  
+};
+
+
+#endif
