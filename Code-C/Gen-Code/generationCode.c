ee605b7c836268f040c5a66caa05bd52329a07d6
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-14 11:30:00 +0100

    Update generationCode.c

--- Code-C/Gen-Code/generationCode.c
+++ Code-C/Gen-Code/generationCode.c
@@ -1,5 +1,13 @@
-
 #include "generationCode.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+int generateCode(int choice)
+{
+	FILE *prm;
+
+	prm = fopen("/pathtoarduino/param.h", "w"); //chance the path when you have it
+	frpintf(prm, "#define CHOICE %d", choice);
+	fclose(prm);
+}
