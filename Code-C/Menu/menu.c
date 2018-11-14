adba9836b073c58376b516dec7c39df971f1c001
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-14 10:28:34 +0100

    Update menu.c

--- Code-C/Menu/menu.c
+++ Code-C/Menu/menu.c
@@ -1,1 +1,9 @@
-
+#include "menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int choix (choice)
+{
+   printf("Quelle manière de s'allumer?\n0:pas de LED\n1:une LED sur deux \n2:une LED sur trois\n3:Toute les LEDs\n5 ou plus:quitter\n");
+   scanf("%d", choice);
+}
