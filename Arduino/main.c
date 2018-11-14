5b3485be9a9566f6cd067d2f790d85ef83417a01
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-14 11:14:14 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -1,1 +1,25 @@
+/**************************************************
 
+ * Capteur infrarouge et le phototransistor permettent de détecter les pulsations cardiaques.
+
+ * On enregistre les données brutes pendant 5s .
+
+ **************************************************/
+
+
+#include "cardio.h"
+
+
+void setup() 
+{
+
+
+}    
+
+
+void loop() 
+{
+ cardio.CardioDetection();
+ cardio.CalculPouls();
+ delay(5000);
+}
