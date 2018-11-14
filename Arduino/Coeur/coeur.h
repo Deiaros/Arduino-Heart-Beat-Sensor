d7b6b50dba28125065d7e2e5e34d5013225f6d80
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-14 16:52:22 +0100

    Update coeur.h

--- Arduino/Coeur/coeur.h
+++ Arduino/Coeur/coeur.h
@@ -1,1 +1,19 @@
+/*
+	Arduino Library that controls & configures and LED heart - coeur.h
+	Created by Thomas B. Varjavandi, November 14, 2018.
+*/
 
+#ifndef COEUR_H
+#define COEUR_H
+
+#include "Arduino.h"
+#include "param.h"
+
+class Coeur
+{
+	public:
+		Coeur();
+		void lightCoeur();
+};
+
+#endif
