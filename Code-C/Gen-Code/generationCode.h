4171be5aab1d512df753e1c2897ec5106c3456ff
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-14 16:36:52 +0100

    Update generationCode.h

--- Code-C/Gen-Code/generationCode.h
+++ Code-C/Gen-Code/generationCode.h
@@ -1,6 +1,14 @@
+/*
+	Library for the generation of an arduino Library - gerationCode.h
+	Created by Thomas B. Varjavandi, November 14, 2018.
+*/
+
 #ifndef GENERATECODE_H
 #define GENERATECODE_H
 
+#define NUMELEM(x)  (sizeof(x) / sizeof((x)[0]))
+
 int generateCode(int choice)
+int printCode()
 
 #endif
