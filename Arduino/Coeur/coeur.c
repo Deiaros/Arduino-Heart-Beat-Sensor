6936c34f4e8797a31c90fcb9efcd401828fc71b8
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-15 16:40:36 +0100

    Update coeur.c

--- Arduino/Coeur/coeur.c
+++ Arduino/Coeur/coeur.c
@@ -7,18 +7,30 @@
 #include "coeur.h"
 #include "param.h"
 
-int pin[10] = {2,3,4,5,6,7,8,9,10,11}
+int pin[10] = {2,3,4,5,6,7,8,9,10,11};
 #define NUMELEM(x)  (sizeof(x) / sizeof((x)[0]))
 
 COEUR::COEUR()
 {
-	for(int i = 2; i < 12)
+	for(int i = 2; i < 12;i++)
 	{
-		pinmode(i, OUTPUT);
+		pinMode(i, OUTPUT);
 	}
 }
 
-void COEUR::lightCoeur()
+void COEUR::checkHeart(int val)
+{
+	if(val == 0)
+	{
+		lightCoeur(HIGH);
+	}
+	else
+	{
+		lightCoeur(LOW);
+	}
+}
+
+void COEUR::lightCoeur(char state)
 {
 	int pmax = NUMELEM(ports)
     for(int i = 2; i <= max; i++)
@@ -27,8 +39,8 @@ void COEUR::lightCoeur()
         {
             if(i == ports[j])
             {
-                digitalWrite(pin[i-2], HIGH);
+                digitalWrite(pin[i-2], state);
             }
         }
-    }
+    }	
 }
