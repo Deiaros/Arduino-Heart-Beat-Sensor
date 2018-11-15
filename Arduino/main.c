d1db663a1b01439579384f715dc4a117e24cb55a
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-16 00:05:29 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -3,19 +3,19 @@
   Created by Thomas B. Varjavandi (Deiaros), November 15, 2018.
 */
 
-#include "coeur.h" //on ajoute notre programme contenant nos prototypes
+#include "coeur.h" //on ajoute notre bibliothèque contenant tous nos prototypes de fonctions
 
-int val = 0; //on crée notre variable qui va récupérer la valeur du "récepteur"
+int val = 0; //on crée notre variable qui va récupérer la valeur du "récepteur IR"
 
-Coeur coeur(); //fonction coeur qui a du coeur
+Coeur coeur(); //fonction coeur qui provient de la classe coeur (elle a du coeur)
 
 void setup() //fonction de préparation...
 {
-	//...qui est vide	
+	//...est agrémentée dans les fichiers qui avaient besoin d'une accrémentation	
 }
 
-void loop()  //fonction que l'arduino va effectuer en continue
+void loop()  //fonction que l'arduino va effectuer en continue (en boucle --> loop==boucle)
 {
-	val = analogRead(A0); 	//on affecte à notre variable, la valeur transmsise par le récepteur
-	checkHeart(val);	//on effectue la fonction checkHeart
+	val = analogRead(A0); 	//on affecte la valeur transmsise par le récepteur IR (et lu par la petite LED du montage cardio) à notre variable 
+	checkHeart(val);	//on effectue la fonction checkHeart avec la valeur obtenue
 }
