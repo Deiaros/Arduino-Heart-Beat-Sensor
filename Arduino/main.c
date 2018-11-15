cd6b5387375d1e2981cb87b98b61fde5705ffb2b
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-15 21:32:37 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -3,19 +3,19 @@
   Created by Thomas B. Varjavandi (Deiaros), November 15, 2018.
 */
 
-#include "coeur.h"
+#include "coeur.h" //on ajoute notre programme contenant nos prototypes
 
-int val = 0;
+int val = 0; //on crée notre variable qui va récupérer la valeur du "récepteur"
 
-Coeur coeur();
+Coeur coeur(); //fonction coeur qui a du coeur
 
-void setup()
+void setup() //fonction de préparation...
 {
-	
+	//...qui est vide	
 }
 
-void loop()
+void loop()  //fonction que l'arduino va effectuer en continue
 {
-	val = analogRead(A0);
-	checkHeart(val);
+	val = analogRead(A0); 	//on affecte à notre variable, la valeur transmsise par le récepteur
+	checkHeart(val);	//on effectue la fonction checkHeart
 }
