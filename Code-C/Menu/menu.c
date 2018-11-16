1381e0bd1b20864115d88c7c9b9d8f2b301b9869
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-16 13:30:37 +0100

    Update menu.c

--- Code-C/Menu/menu.c
+++ Code-C/Menu/menu.c
@@ -4,23 +4,41 @@
    Editied by Thomas B. Varjavandi (Deiaros), November 14, 2018.
 */
 
-#include "menu.h" //on inclus menu.h ainsis que les bibliothèques standard
+#include "menu.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int choix() //fonction choix
+int choix()
 {
-   int choice; //on crée la variable choice
-   printf("Quelle manière de s'allumer?\n0:pas de LED\n1:Toute les LEDs\n2:une LED sur deux\n3:une LED sur trois\n4a Hasard\n5:quitter\n"); // on affiche notre "menu"
-   scanf("%d", choice); //l'utilisateur saisit son choix
-   while(choice > -1 && choice < 6) //si le choix de l'utilisateur n'est pas compris dans l'intervvalle prévu
+   int choice;
+   printf("Quelle manière de s'allumer?");
+   printf("\n[0] : Pas de LED");
+   printf("\n[1] : Toute les LEDs");
+   printf("\n[2] : Toute les LED en chenille");
+   printf("\n[3] : Une LED sur deux");
+   printf("\n[4] : Une LED sur trois");
+   printf("\n[5] : A hasard");
+   printf("\n[6] : A votre choix");
+   printf("\n[7] : Quitter\n");
+
+   scanf("%d", choice);
+   while(choice > -1 || choice < 6)
    {
-      printf("ce n'est pas une option"); //on lui demande (gentiment de refaire son choix
-      sleep(2)
+      printf("ce n'est pas une option");
+      sleep(2);
       system("CLS");
-      printf("Quelle manière de s'allumer?\n0:pas de LED\n1:Toute les LEDs\n2:une LED sur deux\n3:une LED sur trois\n4a Hasard\n5:quitter\n");
+      printf("Quelle manière de s'allumer?");
+      printf("\n[0] : Pas de LED");
+      printf("\n[1] : Toute les LEDs");
+      printf("\n[2] : Toute les LED en chenille");
+      printf("\n[3] : Une LED sur deux");
+      printf("\n[4] : Une LED sur trois");
+      printf("\n[5] : A hasard");
+      printf("\n[6] : A votre choix");
+      printf("\n[7] : Quitter\n");
       scanf("%d", choice);
    }
+
    return choice;
 }
