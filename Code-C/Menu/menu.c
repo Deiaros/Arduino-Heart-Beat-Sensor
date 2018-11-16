98f1514d86525e6b363eb217f691ae629fcee063
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-16 14:17:13 +0100

    Update menu.c

--- Code-C/Menu/menu.c
+++ Code-C/Menu/menu.c
@@ -4,15 +4,15 @@
    Editied by Thomas B. Varjavandi (Deiaros), November 14, 2018.
 */
 
-#include "menu.h"
+#include "menu.h" //bibliothèques
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int choix()
+int choix() //fonction choix
 {
-   int choice;
-   printf("Quelle manière de s'allumer?");
+   int choice; //on crée la variable choice 
+   printf("Quelle manière de s'allumer?"); //on demande à notre utilisateur de rentrer l'allumage de LED qu'il veut
    printf("\n[0] : Pas de LED");
    printf("\n[1] : Toute les LEDs");
    printf("\n[2] : Toute les LED en chenille");
@@ -23,9 +23,9 @@ int choix()
    printf("\n[7] : Quitter\n");
 
    scanf("%d", choice);
-   while(choice > -1 || choice < 6)
+   while(choice > -1 || choice < 6) //si son choix est impossible, on lui demande (gentiment) de le refaire
    {
-      printf("ce n'est pas une option");
+      printf("ce n'est pas une option!!!");
       sleep(2);
       system("CLS");
       printf("Quelle manière de s'allumer?");
