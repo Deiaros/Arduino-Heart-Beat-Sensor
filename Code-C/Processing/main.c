d53fcd3fb7d4a3423fba5cfe6f62c53fb09e7d44
Joana64 <45042443+Joana64@users.noreply.github.com>
2018-11-16 19:08:37 +0100

    Create main.c

---
+++ Code-C/Processing/main.c
@@ -0,0 +1,19 @@
+/*--------------
+
+  *Created by :
+    * Joana Cazaurang
+    * Thomas Varjavandi
+    * Antoine Merlingeas
+
+-----------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <menu.h>
+
+int choix;
+
+void main()
+{
+    menu(choix);
+}
