de9e43be089e6ada83e6d900ec7bad626318be22
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-16 10:30:54 +0100

    Update generationCode.h

--- Code-C/Gen-Code/generationCode.h
+++ Code-C/Gen-Code/generationCode.h
@@ -3,12 +3,12 @@
 	Created by Thomas B. Varjavandi (Deiaros), November 14, 2018.
 */
 
-#ifndef GENERATECODE_H
-#define GENERATECODE_H
+#ifndef GENERATECODE_H 
+#define GENERATECODE_H //On défini GENERATECODE_H
 
-#define NUMELEM(x)  (sizeof(x) / sizeof((x)[0]))
+#define NUMELEM(x)  (sizeof(x) / sizeof((x)[0])) //on défini NUMELEM(x) défini par la taille des tableaux précédemment crée
 
-int generateCode(int choice)
-int printCode()
+int generateCode(int choice) //on crée la fonction generateCode comprenant la variable choice
+int printCode() //création de la fonction printCode ()
 
 #endif
