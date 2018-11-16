b3f6430dad8ef7953bfe1d559113f429c29966e4
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-16 13:56:04 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -4,31 +4,31 @@
 */
 #include "coeur.h" //on inclus la bibliothèque que nous avons crée
 
-Coeur Coeur;
+Coeur Coeur;//on va chercher la fonction coeur dans la classe coeur
 
-int val = 0;
-int check = 0;
+int val = 0; //on initie la variable val
+int check = 0; //on initie la variable check
 
-void setup()
+void setup() // fonction de préparation
 {
- Serial.begin(9600);
+ Serial.begin(9600); //on alloue la mémoire
 }
 
-void loop()
+void loop() // fonction que l'arduino va faire en boucle
 {
-  val = analogRead(A0);
-  Serial.println(val);
-  if(val < 200)
+  val = analogRead(A0); //on affecte à val la valeur du port A0
+  Serial.println(val); //on affiche la val
+  if(val < 200) // si val est inférieur à 200
   {
-    if(check == 0)
+    if(check == 0) // et si check équivaut à 0 (éteint)
     {
-      Coeur.lightCoeur(HIGH);
-      check = 1;
+      Coeur.lightCoeur(HIGH); //on allume notre coeur de LED
+      check = 1; //puis on mets check à 1 (allumé)
     }
   }
   else
   {
-    Coeur.lightCoeur(LOW);
-    check = 0;
+    Coeur.lightCoeur(LOW); //sinon on éteins
+    check = 0; //puis on met check à 0 (éteins)
   }
 }
