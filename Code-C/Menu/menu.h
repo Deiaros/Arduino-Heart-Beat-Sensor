24a5f2fcaa4c8a242e2db927bc092740e2d77271
Orme21 <44492129+Orme21@users.noreply.github.com>
2018-11-16 11:25:21 +0100

    Update menu.h

--- Code-C/Menu/menu.h
+++ Code-C/Menu/menu.h
@@ -1,9 +1,9 @@
 #ifndef MENU_H
-#define MENU_H
-#include <stdio.h>
+#define MENU_H //on crée MENU_H
+#include <stdio.h> //dans lesquels seront inclus les bibliothèques standard du langage C 
 #include <stdlib.h>
 #include <time.h>
 
-int choix ()
+int choix () //on crée la variable choix
 
 #endif
