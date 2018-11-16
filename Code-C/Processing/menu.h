2f866ee70b5d842b18b45fbe1e187ca4cf0e67ee
Joana64 <45042443+Joana64@users.noreply.github.com>
2018-11-16 19:06:15 +0100

    Create menu.h

---
+++ Code-C/Processing/menu.h
@@ -0,0 +1,14 @@
+/*-------------------
+
+    * Created by Joana Cazaurang
+
+--------------------*/
+
+#ifndef MENU_H
+#define MENU_H
+
+
+void menu(int choice);
+void menu2(int choice);
+
+#endif // MENU_H
