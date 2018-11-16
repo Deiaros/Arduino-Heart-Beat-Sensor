ba6e2d6cd43c0d27b6e94275d15d1b8db734fa34
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-16 13:43:58 +0100

    Update main.c

--- Code-C/main.c
+++ Code-C/main.c
@@ -1,1 +1,20 @@
+/*
 
+	Main file that starts the menu and code generator - main.c
+	Created by Thomas B. Varjavandi (Deiaros), November 16, 2018.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "menu.h"
+#include "generateCode.h"
+
+int choice;
+
+void main()
+{
+	choice = choix();
+	generateCode(choice);
+}
