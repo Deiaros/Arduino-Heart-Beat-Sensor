ae11540667039cf595fdc61d3d269c036792e88f
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-19 10:17:08 +0100

    Update donnees.c

--- Code-C/Libraries/donnees.c
+++ Code-C/Libraries/donnees.c
@@ -1,1 +1,18 @@
+#include "donnees.h"
 
+int readFile(Pouls *beat)
+{
+	int i = 0;
+	char chain[20];
+
+	FILE *poof;
+
+	poof = fopen("Battements.csv", "r");
+
+	while(fgets(chain, 500, poof)!=NULL)
+	{
+		scanf(chain, "%lf;%d", &beat[i].mil, &beat[i].pou);
+		i++;
+	}
+	return i;
+}
