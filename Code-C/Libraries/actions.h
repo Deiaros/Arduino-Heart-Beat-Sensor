bf7f7672371470f7897502e4820f02e4c683d28e
Deiaros <38224402+Deiaros@users.noreply.github.com>
2018-11-19 10:16:45 +0100

    Update actions.h

--- Code-C/Libraries/actions.h
+++ Code-C/Libraries/actions.h
@@ -1,1 +1,16 @@
+#ifndef actions_h
+#define actions_h
+#include "donnees.h"
 
+void printFile(Pouls *beat,int count);
+void printTimeCroissant(Pouls *beat,int count);
+void printTimeDecroissant(Pouls *beat,int count);
+void printPulseCroissant(Pouls *beat,int count);
+void printPulseDecroissant(Pouls *beat,int count);
+void printSpecificTime(Pouls *beat,int count);
+void printMeanPulseBetweenTime(Pouls *beat,int cmpt);
+void printMeanPulse(Pouls *beat,int count);
+void printCountLines(Pouls *beat,int count);
+void printPulseMinMax(Pouls *beat,int count);
+
+#endif
